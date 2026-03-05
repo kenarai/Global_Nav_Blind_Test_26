d5a1a4842f4c564af64c29e05d953ae1d567bb61
Ken Arai <hi@kenarai.com>
2026-03-05 11:54:47 -0800

    -

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,9 @@ import './styles/global.css';
 
 document.title = `Option ${__ACTIVE_OPTION__}`;
 
-const basename = __ACTIVE_OPTION__ === '2' ? '/opt2' : __ACTIVE_OPTION__ === '3' ? '/opt3' : '/';
+const basename = import.meta.env.DEV
+  ? '/'
+  : __ACTIVE_OPTION__ === '2' ? '/opt2' : __ACTIVE_OPTION__ === '3' ? '/opt3' : '/';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
